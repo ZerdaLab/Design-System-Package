9f872eb0de02c1cb963e3d52327b1b5d4f9a80f4
Jonathan Stevens <jonathan@resnovas.com>
2022-09-13 14:35:56 +0100

    feat: version 1 design system package
    Signed-off-by: Jonathan Stevens <jonathan@resnovas.com>

---
+++ build/ios/Classes/Generated/StyleDictionaryProperties.h
@@ -0,0 +1,19 @@
+
+//
+// StyleDictionaryProperties.h
+//
+
+// Do not edit directly
+// Generated on Tue, 13 Sep 2022 13:21:43 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface StyleDictionaryProperties : NSObject
+
++ (NSDictionary *)properties;
++ (NSDictionary *)getProperty:(NSString *)keyPath;
++ (nonnull)getValue:(NSString *)keyPath;
+
+@end
